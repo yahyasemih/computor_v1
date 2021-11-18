9b07b11d769ca23c37a455f16188a52ae18f6fca
Yahya EZ-ZAINABI <yez-zain@student.1337.ma>
2021-11-18 20:17:53 +0100

    Remove unused utilities

--- utilities.cpp
+++ utilities.cpp
@@ -6,24 +6,14 @@
 /*   By: yez-zain <yez-zain@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 16:36:31 by yez-zain          #+#    #+#             */
-/*   Updated: 2021/11/18 19:39:32 by yez-zain         ###   ########.fr       */
+/*   Updated: 2021/11/18 20:17:15 by yez-zain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utilities.hpp"
 
-bool is_number(const std::string &str) {
-	return std::all_of(str.cbegin(), str.cend(),
-		[](char c){ return ::isdigit(c); });
-}
-
 bool is_operator(char c) {
 	return c == '+' || c == '-' || c == '*' || c == '^' || c == '=';
-}
-
-bool is_identifier(const std::string &str) {
-	return std::all_of(str.cbegin(), str.cend(),
-		[](char c){ return ::isalpha(c); });
 }
 
 double my_sqrt(double d) {
--- utilities.hpp
+++ utilities.hpp
@@ -6,7 +6,7 @@
 /*   By: yez-zain <yez-zain@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 16:29:33 by yez-zain          #+#    #+#             */
-/*   Updated: 2021/11/18 16:46:50 by yez-zain         ###   ########.fr       */
+/*   Updated: 2021/11/18 20:17:35 by yez-zain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@
 #include <string>
 #include <algorithm>
 
-bool is_number(const std::string &str);
 bool is_operator(char c);
-bool is_identifier(const std::string &str);
+
 double my_sqrt(double d);
 
 #endif // UTILITIES_HPPß
