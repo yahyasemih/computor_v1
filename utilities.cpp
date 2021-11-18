e83046ccaad198cb8b91ff1cb0a3f74ef4e98474
Yahya EZ-ZAINABI <yez-zain@student.1337.ma>
2021-11-18 18:17:53 +0100

    Implement sqrt

--- utilities.cpp
+++ utilities.cpp
@@ -6,13 +6,11 @@
 /*   By: yez-zain <yez-zain@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 16:36:31 by yez-zain          #+#    #+#             */
-/*   Updated: 2021/11/18 17:59:38 by yez-zain         ###   ########.fr       */
+/*   Updated: 2021/11/18 18:13:20 by yez-zain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utilities.hpp"
-#include <cmath>
-#include <iostream>
 
 bool is_number(const std::string &str) {
 	return std::all_of(str.cbegin(), str.cend(), [](char c){ return ::isdigit(c); });
@@ -27,5 +25,18 @@ bool is_identifier(const std::string &str) {
 }
 
 double my_sqrt(double d) {
-	return sqrt(d);
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = d * 0.5F;
+	y  = d;
+	i  = * ( long * ) &y;
+	i  = 0x5f3759df - ( i >> 1 );
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );
+	y  = y * ( threehalfs - ( x2 * y * y ) );
+	y  = y * ( threehalfs - ( x2 * y * y ) );
+
+	return -1/y;
 }
